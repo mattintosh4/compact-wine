417c5732b8da374452dfc30400502cf2cae17307
mattintosh4 <mattintosh4@gmail.com>
2014-02-09 05:51:00 +0900

    no message

--- build.sh
+++ build.sh
@@ -402,10 +402,112 @@ index 3ee8473..19d2f6c 100644
 
 
 
+diff --git a/programs/explorer/explorer.c b/programs/explorer/explorer.c
+index 1b900d0..23a4a84 100644
+--- a/programs/explorer/explorer.c
++++ b/programs/explorer/explorer.c
+@@ -44,8 +44,8 @@ WINE_DEFAULT_DEBUG_CHANNEL(explorer);
+ #define NAV_TOOLBAR_HEIGHT 30
+ #define PATHBOX_HEIGHT 24
+ 
+-#define DEFAULT_WIDTH 640
+-#define DEFAULT_HEIGHT 480
++#define DEFAULT_WIDTH 800
++#define DEFAULT_HEIGHT 600
+ 
+ 
+ static const WCHAR EXPLORER_CLASS[] = {'W','I','N','E','_','E','X','P','L','O','R','E','R','\0'};
+
+
+
+diff --git a/dlls/winemac.drv/keyboard.c b/dlls/winemac.drv/keyboard.c
+index a46d324..66cc4f2 100644
+--- a/dlls/winemac.drv/keyboard.c
++++ b/dlls/winemac.drv/keyboard.c
+@@ -238,14 +238,14 @@ static const struct {
+     { VK_BACK,                  0x0E,           TRUE },     /* kVK_Delete */
+     { 0,                        0,              FALSE },    /* 0x34 unused */
+     { VK_ESCAPE,                0x01,           TRUE },     /* kVK_Escape */
+-    { VK_RMENU,                 0x38 | 0x100,   TRUE },     /* kVK_RightCommand */
+-    { VK_LMENU,                 0x38,           TRUE },     /* kVK_Command */
++    { 0,                        0,              FALSE },    /* kVK_RightCommand */
++    { 0,                        0,              FALSE },    /* kVK_Command */
+     { VK_LSHIFT,                0x2A,           TRUE },     /* kVK_Shift */
+     { VK_CAPITAL,               0x3A,           TRUE },     /* kVK_CapsLock */
+-    { 0,                        0,              FALSE },    /* kVK_Option */
++    { VK_LMENU,                 0x38,           TRUE },     /* kVK_Option */
+     { VK_LCONTROL,              0x1D,           TRUE },     /* kVK_Control */
+     { VK_RSHIFT,                0x36,           TRUE },     /* kVK_RightShift */
+-    { 0,                        0,              FALSE },    /* kVK_RightOption */
++    { VK_RMENU,                 0x38 | 0x100,   TRUE },     /* kVK_RightOption */
+     { VK_RCONTROL,              0x1D | 0x100,   TRUE },     /* kVK_RightControl */
+     { 0,                        0,              FALSE },    /* kVK_Function */
+     { VK_F17,                   0x68,           TRUE },     /* kVK_F17 */
+
+
+
+diff --git a/dlls/winemac.drv/cocoa_app.m b/dlls/winemac.drv/cocoa_app.m
+index d499fd1..d7dec01 100644
+--- a/dlls/winemac.drv/cocoa_app.m
++++ b/dlls/winemac.drv/cocoa_app.m
+@@ -223,7 +223,6 @@ - (void) transformProcessToForeground
+             item = [submenu addItemWithTitle:title action:@selector(hide:) keyEquivalent:@""];
+ 
+             item = [submenu addItemWithTitle:@"Hide Others" action:@selector(hideOtherApplications:) keyEquivalent:@"h"];
+-            [item setKeyEquivalentModifierMask:NSCommandKeyMask | NSAlternateKeyMask];
+ 
+             item = [submenu addItemWithTitle:@"Show All" action:@selector(unhideAllApplications:) keyEquivalent:@""];
+ 
+@@ -234,7 +233,6 @@ - (void) transformProcessToForeground
+             else
+                 title = @"Quit";
+             item = [submenu addItemWithTitle:title action:@selector(terminate:) keyEquivalent:@"q"];
+-            [item setKeyEquivalentModifierMask:NSCommandKeyMask | NSAlternateKeyMask];
+             item = [[[NSMenuItem alloc] init] autorelease];
+             [item setTitle:@"Wine"];
+             [item setSubmenu:submenu];
+@@ -247,7 +245,6 @@ - (void) transformProcessToForeground
+             if ([NSWindow instancesRespondToSelector:@selector(toggleFullScreen:)])
+             {
+                 item = [submenu addItemWithTitle:@"Enter Full Screen" action:@selector(toggleFullScreen:) keyEquivalent:@"f"];
+-                [item setKeyEquivalentModifierMask:NSCommandKeyMask | NSAlternateKeyMask | NSControlKeyMask];
+             }
+             [submenu addItem:[NSMenuItem separatorItem]];
+             [submenu addItemWithTitle:@"Bring All to Front" action:@selector(arrangeInFront:) keyEquivalent:@""];
+
+
+
 !
 
-    $SED -i '' -e '/^wine_fn_config_program/s/,installbin//' configure
-    $SED -i '' -e '/^wine-installed: main.o wine_info.plist/{n;s|$| -Wl,-rpath,/opt/X11/lib -Wl,-rpath,/usr/lib|;}' configure
+    $SED "s|@WINE_VERSION@|$(< VERSION)|
+          s|@UNAME@|$(uname -v)|
+          s|@TRIPLE@|$TRIPLE|
+          s|@CONFIGURE_ARGS@|${configure_args[*]}|" <<\! | $PATCH -Np1
+diff --git a/loader/main.c b/loader/main.c
+index ac67290..7cdafeb 100644
+--- a/loader/main.c
++++ b/loader/main.c
+@@ -87,6 +87,11 @@ static inline void reserve_area( void *addr, size_t size )
+ static void check_command_line( int argc, char *argv[] )
+ {
+     static const char usage[] =
++        "@WINE_VERSION@  Nihonshu binary edition\n"
++        "  Build: @UNAME@\n"
++        "  Target: @TRIPLE@\n"
++        "  Configured with: @CONFIGURE_ARGS@\n"
++        "\n"
+         "Usage: wine PROGRAM [ARGUMENTS...]   Run the specified program\n"
+         "       wine --help                   Display this help and exit\n"
+         "       wine --version                Output version information and exit";
+!
+
+    $SED -i '' "
+        /^wine_fn_config_program/s|,installbin||
+        /^wine-installed: main.o wine_info.plist/{
+            n
+            s|\$| -Wl,-rpath,$XLIBDIR -Wl,-rpath,/usr/lib|
+        }
+    " configure
 }
 
 #-------------------------------------------------------------------------------
