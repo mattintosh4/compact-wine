9bdf0b3056523375595b0bac4d09ccfd9be3cdc8
mattintosh4 <mattintosh4@gmail.com>
2014-06-02 16:15:16 +0900

    .

--- build.sh
+++ build.sh
@@ -1,12 +1,18 @@
-#!/usr/bin/env - SHELL=/bin/sh TERM=xterm-256color COMMAND_MODE=unix2003 LANG=C LC_ALL=C /bin/ksh
+#!/usr/bin/env - PATH=/usr/bin:/bin:/usr/sbin:/sbin SHELL=/bin/sh TERM=xterm-256color COMMAND_MODE=unix2003 LANG=C LC_ALL=C /bin/ksh
 set -e
 set -o pipefail
 set -u
 set -x
 
-CS_PATH=/usr/bin:/bin:/usr/sbin:/sbin
-PATH=$CS_PATH
-export PATH
+__CS_PATH__=/usr/bin:/bin:/usr/sbin:/sbin
+__SDKROOT__=/Developer/SDKs/MacOSX10.6.sdk
+__TRIPLE__=i686-apple-darwin`uname -r`
+__MACPORTSPREFIX__=/opt/local
+__TOOLPREFIX__AUTOTOOLS__=$__MACPORTSPREFIX__/libexec/autotools
+__TOOLPREFIX__CCACHE__=$__MACPORTSPREFIX__/libexec/ccache
+__TOOLPREFIX__GETTEXT__=$__MACPORTSPREFIX__/libexec/gettext
+__TOOLPREFIX__GIT__=$__MACPORTSPREFIX__/libexec/git-core
+__TOOLPREFIX__XZ__=$__MACPORTSPREFIX__/libexec/xz
 
 define(){ eval ${1}='"${*:2}"'; }
 
@@ -27,104 +33,55 @@ define INCDIR           ${PREFIX}/include
 define LIBDIR           ${W_PREFIX}/lib
 define DATADIR          ${PREFIX}/share
 
-define MACPORTSDIR      /opt/local
 define XDIR             /opt/X11
 define XINCDIR          ${XDIR}/include
 define XLIBDIR          ${XDIR}/lib
 
-PATH=\
-${BINDIR}:\
-${MACPORTSDIR}/libexec/ccache:\
-${MACPORTSDIR}/libexec/git-core:\
-$CS_PATH
-
-AC_PATH=\
-${MACPORTSDIR}/libexec/gnubin:\
-${MACPORTSDIR}/bin:\
-${MACPORTSDIR}/sbin:\
-$CS_PATH
-
-set -- `uname -r`
-DARWIN_VERSION=$1
-
-SFI=$IFS IFS=.
-set -- `sw_vers -productVersion`
-IFS=$SFI
-MACOSX_DEPLOYMENT_TARGET=$1.$2
-export MACOSX_DEPLOYMENT_TARGET
-
-SFI=$IFS IFS=$'\n'
-set -- `xcodebuild -version -sdk macosx$MACOSX_DEPLOYMENT_TARGET`
-IFS=$SFI
-for r
-{
-    set -- $r
-    case $1 in
-    Path:)
-        SDKROOT=$2
-        break
-        ;;
-    esac
-}
-
-TRIPLE=i686-apple-darwin$DARWIN_VERSION
+PATH=
+PATH+=:$__TOOLPREFIX__CCACHE__
+PATH+=:$__TOOLPREFIX__GIT__
+PATH+=:$__TOOLPREFIX__GETTEXT__
+PATH+=:$__TOOLPREFIX__AUTOTOOLS__
+PATH+=:$__TOOLPREFIX__XZ__
+PATH+=:$__CS_PATH__
 
 set -a
-define CC                   gcc-apple-4.2
-define CXX                  g++-apple-4.2
-define CFLAGS               -m32 -arch i386 -O3 -march=core2 -mtune=core2
-define CPPFLAGS             -isysroot ${SDKROOT} -I${INCDIR}
-define CXXFLAGS             ${CFLAGS}
-define CXXCPPFLAGS          ${CPPFLAGS}
-define LDFLAGS              -Wl,-headerpad_max_install_names -Wl,-syslibroot,${SDKROOT} -arch i386 -Z -L${LIBDIR} -L/usr/lib -F/System/Library/Frameworks
+CCACHE_PATH=
+CCACHE_PATH+=:$__MACPORTSPREFIX__/bin
+CC=gcc-apple-4.2
+CXX=g++-apple-4.2
 
-define CCACHE_PATH          ${MACPORTSDIR}/bin
+CFLAGS="-m32 -arch i386 -O3 -march=core2 -mtune=core2"
+CXXFLAGS=$CFLAGS
+CPPFLAGS=
+CPPFLAGS+=" -isysroot $__SDKROOT__"
+CPPFLAGS+=" -I$INCDIR"
+LDFLAGS=" -arch i386"
+LDFLAGS+=" -Wl,-headerpad_max_install_names"
+LDFLAGS+=" -Wl,-syslibroot,$__SDKROOT__ "
+LDFLAGS+=" -Z -L$LIBDIR -L/usr/lib -F/System/Library/Frameworks"
 
-define FONTFORGE            ${MACPORTSDIR}/bin/fontforge
-define INSTALL_NAME_TOOL    ${MACPORTSDIR}/bin/install_name_tool
-define MAKE                 ${MACPORTSDIR}/bin/gmake
-define MSGFMT               ${MACPORTSDIR}/bin/msgfmt
-define NASM                 ${MACPORTSDIR}/bin/nasm
-define PKG_CONFIG           ${MACPORTSDIR}/bin/pkg-config
-define PKG_CONFIG_LIBDIR    ${LIBDIR}/pkgconfig:/usr/lib/pkgconfig
+INSTALL_NAME_TOOL=$__MACPORTSPREFIX__/bin/install_name_tool
+
+PKG_CONFIG=$__MACPORTSPREFIX__/bin/pkg-config
+PKG_CONFIG_LIBDIR=
+PKG_CONFIG_LIBDIR+=:$LIBDIR/pkgconfig
+PKG_CONFIG_LIBDIR+=:/usr/lib/pkgconfig
 set +a
-
-for f in \
-INSTALL_NAME_TOOL \
-MAKE \
-MSGFMT \
-NASM \
-PKG_CONFIG
-{
-    eval test -x '$'$f
-}
 
 #-------------------------------------------------------------------------------
 
-autogen()
-{
-    PATH=${AC_PATH} NOCONFIGURE=1 ./autogen.sh ${@-}
-}
-autoreconf()
-{
-    PATH=${AC_PATH} NOCONFIGURE=1 command autoreconf -i ${@-}
-}
-configure()
-{
-    ./configure --prefix=${PREFIX} --build=${TRIPLE} --libdir=${LIBDIR} ${@-}
-}
 git_checkout()
 {
-    git checkout -f ${1-master}
-}
-make_install()
-{
-    $MAKE --jobs=3
-    $MAKE install
+    : ${1:?}
+
+    git checkout -f -B temp $1
 }
 clone_repos()
 {
-    rsync -a --delete ${SRCROOT}/${1}/ ${TMPDIR}/${1}
+    : ${1:?}
+
+    rsync -a --delete ${SRCROOT}/./${1}/ ${TMPDIR}/${1}
     cd ${TMPDIR}/${1}
 }
 
@@ -132,102 +89,119 @@ clone_repos()
 
 build_freetype()
 {
-    typeset name
-    name=freetype
-
-    clone_repos ${name}
-    git_checkout
-    autogen
-    configure --disable-static
-    make_install
+    clone_repos freetype
+    git_checkout remotes/origin/master
+    ./autogen.sh
+    args=(
+        --prefix=$PREFIX
+        --libdir=$LIBDIR
+        --disable-static
+    )
+    ./configure ${args[@]}
+    make -j3
+    make install
 }
-
 build_xz()
 {
-    typeset name
-    name=xz
-
-    clone_repos ${name}
-    git_checkout v5.0
-    autogen
-    configure \
-        --disable-dependency-tracking \
-        --disable-nls \
+    clone_repos xz
+    git_checkout remotes/origin/v5.0
+    ./autogen.sh
+    args=(
+        --prefix=$PREFIX
+        --libdir=$LIBDIR
+        --disable-dependency-tracking
+        --disable-nls
         --disable-static
-    make_install
+        --disable-xz
+        --disable-xzdec
+        --disable-lzmadec
+        --disable-lzmainfo
+        --disable-lzma-links
+        --disable-scripts
+    )
+    ./configure ${args[@]}
+    make -j3
+    make install
 }
 
-build_jpeg()
+build_libjpeg()
 {
-    typeset name
-    name=libjpeg-turbo
-
-    clone_repos ${name}
-    git_checkout 1.3.x
-    sed -i '' '\|$(datadir)/doc|s||&/libjpeg-turbo|' Makefile.am
-    autoreconf
-    configure \
-        --disable-dependency-tracking \
-        --disable-static \
+    clone_repos libjpeg-turbo
+    git_checkout remotes/1.3.x
+    autoreconf -vi
+    args=(
+        --prefix=$PREFIX
+        --libdir=$LIBDIR
+        --build=$__TRIPLE__
+        --disable-dependency-tracking
+        --disable-static
         --with-jpeg8
-    make_install
+    )
+    configure ${args[@]}
+    make -j3 V=1
+    make install
 }
 
-build_tiff()
+build_libtiff()
 {
-    typeset name
-    name=libtiff
-
-    clone_repos ${name}
-    git_checkout branch-3-9
-    configure \
-        --disable-dependency-tracking \
-        --disable-cxx \
-        --disable-jbig \
-        --disable-static \
-        --with-apple-opengl-framework \
-        --with-x \
-        --x-inc=${XINCDIR} \
+    clone_repos libtiff
+    git_checkout master
+    args=(
+        --prefix=$PREFIX
+        --libdir=$LIBDIR
+        --disable-dependency-tracking
+        --disable-cxx
+        --disable-jbig
+        --disable-static
+        --with-apple-opengl-framework
+        --with-x
+        --x-inc=${XINCDIR}
         --x-lib=${XLIBDIR}
-    make_install
+    )
+    ./configure ${args[@]}
+    make -j3
+    make install
 }
 
 build_lcms()
 {
-    typeset name
-    name=Little-CMS
-
-    clone_repos ${name}
-    git_checkout
-    configure \
-        --disable-dependency-tracking \
+    clone_repos Little-CMS
+    git_checkout remotes/origin/master
+    args=(
+        --prefix=$PREFIX
+        --libdir=$LIBDIR
+        --disable-dependency-tracking
         --disable-static
-    make_install
+    )
+    ./configure ${args[@]}
+    make -j3
+    make install
 }
 
-build_png()
+build_libpng()
 {
-    typeset name
-    name=libpng
-
-    clone_repos ${name}
-    git_checkout libpng16
-    autogen
-    configure \
-        --disable-dependency-tracking \
+    clone_repos libpng
+    git_checkout remotes/origin/libpng16
+    ./autogen.sh
+    args=(
+        --prefix=$PREFIX
+        --libdir=$LIBDIR
+        --disable-dependency-tracking
         --disable-static
-    make_install
+    )
+    ./configure ${args[@]}
+    make -j3
+    make install
 }
 
 build_wine()
 {
-    typeset name
-    name=wine
-
-    typeset configure_args
-    configure_args=(
+    clone_repos wine
+    git_checkout remotes/origin/master
+    ## This variable is needed patching.
+    args=(
         --prefix=${W_PREFIX}
-        --build=${TRIPLE}
+        --build=$__TRIPLE__
         --disable-win16
         --with-cms
         --with-coreaudio
@@ -252,13 +226,10 @@ build_wine()
         --without-sane
         --without-v4l
     )
-
-    clone_repos ${name}
-    git_checkout
     patch_wine
-    git diff > $PROJECTROOT/wine___all.diff
-    ./configure "${configure_args[@]}"
-    make_install
+    ./configure ${args[@]}
+    make -j3
+    make install
 }
 
 #-------------------------------------------------------------------------------
@@ -269,90 +240,135 @@ patch_wine()
     for f
     {
         test -f "$f" || continue
-        patch -Np1 < $f
+        patch -Np1 < "$f"
     }
 
     . $PROJECTROOT/patch_wine.sh
+    git diff > $PROJECTROOT/wine___all.diff
 }
 
 #-------------------------------------------------------------------------------
 
-rm -rf   ${INSTALL_PREFIX}
-rm -rf   ${TMPDIR}
-
-mkdir -p ${INSTALL_PREFIX}
-mkdir -p ${TMPDIR}
-mkdir -p ${W_BINDIR}
-mkdir -p ${W_INCDIR}
-mkdir -p ${W_LIBDIR}
-mkdir -p ${BINDIR}
-mkdir -p ${INCDIR}
-mkdir -p ${LIBDIR}
-
-build_xz
-build_png
-build_jpeg
-build_tiff
-build_freetype
-build_lcms
-build_wine
-
-
-change_id()
+init()
 {
-    src=$1
-    set -- $(otool -XD $src)
-    case $1 in
-    $LIBDIR/*)
-        (
-            set -x
-            $INSTALL_NAME_TOOL -id @rpath/${1##*/} $src
-        )
-        ;;
-    esac
+    rm -rf   ${INSTALL_PREFIX}
+    rm -rf   ${TMPDIR}
+
+    mkdir -p ${INSTALL_PREFIX}
+    mkdir -p ${TMPDIR}
+    mkdir -p ${W_BINDIR}
+    mkdir -p ${W_INCDIR}
+    mkdir -p ${W_LIBDIR}
+    mkdir -p ${BINDIR}
+    mkdir -p ${INCDIR}
+    mkdir -p ${LIBDIR}
 }
-change_link()
+
+make_distfile()
 {
-    src=$1
-    IFS=$'\n'
-    set -- $(otool -XL $src)
-    IFS=$' \t\n'
-    for r
+    change_id()
     {
-        set -- $r
+        src=$1
+        set -- $(otool -XD $src)
         case $1 in
         $LIBDIR/*)
             (
                 set -x
-                $INSTALL_NAME_TOOL -change $1 @rpath/${1##*/} $src
+                $INSTALL_NAME_TOOL -id @rpath/${1##*/} $src
             )
             ;;
         esac
     }
+    change_link()
+    {
+        src=$1
+        IFS=$'\n'
+        set -- $(otool -XL $src)
+        IFS=$' \t\n'
+        for r
+        {
+            set -- $r
+            case $1 in
+            $LIBDIR/*)
+                (
+                    set -x
+                    $INSTALL_NAME_TOOL -change $1 @rpath/${1##*/} $src
+                )
+                ;;
+            esac
+        }
+    }
+
+    set +x
+    IFS=$'\n'
+    set -- `find -L $LIBDIR -type f \( -name "*.dylib" -o -name "*.so" \)`
+    IFS=$' \t\n'
+    for f
+    {
+        case $f in
+        *.dylib)
+            change_id $f
+            ;;
+        esac
+        change_link $f
+    }
+    set -x
+
+    rm -rf $LIBDIR/*.la
+    rm -rf $LIBDIR/pkgconfig
+    
+    mkdir -p          $W_PREFIX/libexec
+    mv $W_BINDIR/wine $W_PREFIX/libexec
+    ln $PROJECTROOT/wineloader.sh.in $W_BINDIR/wine
+    mkdir -p                                  $W_DATADIR/wine/inf
+    ln $PROJECTROOT/osx-wine-inf/osx-wine.inf $W_DATADIR/wine/inf
+    
+    $PROJECTROOT/patch_autogen.sh $PROJECTROOT
 }
 
-set +x
-IFS=$'\n'
-set -- `find -L $LIBDIR -type f \( -name "*.dylib" -o -name "*.so" \)`
-IFS=$' \t\n'
-for f
-{
-    case $f in
-    *.dylib)
-        change_id $f
-        ;;
-    esac
-    change_link $f
-}
-set -x
-
-rm -rf $LIBDIR/*.la
-rm -rf $LIBDIR/pkgconfig
-
-mkdir -p          $W_PREFIX/libexec
-mv $W_BINDIR/wine $W_PREFIX/libexec
-ln $PROJECTROOT/wineloader.sh.in $W_BINDIR/wine
-mkdir -p                                  $W_DATADIR/wine/inf
-ln $PROJECTROOT/osx-wine-inf/osx-wine.inf $W_DATADIR/wine/inf
-
-$PROJECTROOT/patch_autogen.sh $PROJECTROOT
+select n in \
+wine-all \
+wine \
+xz \
+libpng \
+freetype \
+libjpeg \
+libtiff \
+lcms
+do
+case ${n:-REPLY} in
+wine-all)
+    init
+    build_xz
+    build_libpng
+    build_freetype
+    build_libjpeg
+    build_libtiff
+    build_lcms
+    build_wine
+    make_distfile
+    ;;
+wine)
+    build_wine
+    ;;
+xz)
+    build_xz
+    ;;
+libpng)
+    build_libpng
+    ;;
+libjpeg)
+    build_libjpeg
+    ;;
+libtiff)
+    build_libtiff
+    ;;
+lcms)
+    build_lcms
+    ;;
+'')
+    continue
+    ;;
+esac
+exit
+done
