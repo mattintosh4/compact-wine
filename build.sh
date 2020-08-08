0dd486ee14fde69c850b965ee8afd3b0a79d6ed3
mattintosh4 <mattintosh4@gmail.com>
2020-08-09 01:31:18 +0900

    updated Wine 5.14

--- build.sh
+++ build.sh
@@ -55,7 +55,7 @@ build_wine()
             source=origin/stable
         ;;
         development)
-            source=wine-5.13
+            source=wine-5.14
         ;;
         staging)
             source=origin/master
